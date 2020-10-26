f3460878c9f32349d962861b7c9634bc7bdbbe25
XuShenLZ <xushen0118@qq.com>
2020-10-26 01:04:52 -0700

    plot and extract frames from rosbag

--- bag_processing/bag_utils.py
+++ bag_processing/bag_utils.py
@@ -9,41 +9,44 @@ class Vehicle(object):
         """
         Initializing
         """
-        self.t0 = None
-        self.t = []
+        self.state_t0 = None
+        self.state_t = []
 
         self.x = []
         self.y = []
         self.heading = []
         self.v = []
 
+        self.input_t0 = None
+        self.input_t = []
         self.delta = []
         self.a = []
 
         self.pred = None
-
-    def append_time(self, t):
-        """
-        append current time
-        """
-        if self.t0 == None:
-            self.t0 = t
-            
-        self.t.append(t-self.t0)
     
-    def append_state(self, msg):
+    def append_state(self, msg, t):
         """
         append vehicle state
         """
+        if self.state_t0 == None:
+            self.state_t0 = t
+        
+        self.state_t.append(t - self.state_t0)
+
         self.x.append(msg.x)
         self.y.append(msg.y)
         self.heading.append(msg.psi)
         self.v.append(msg.v)
 
-    def append_input(self, msg):
+    def append_input(self, msg, t):
         """
         append vehicle input
         """
+        if self.input_t0 == None:
+            self.input_t0 = t
+        
+        self.input_t.append(t - self.input_t0)
+
         self.delta.append(msg.servo)
         self.a.append(msg.motor)
 
@@ -54,22 +57,47 @@ class Vehicle(object):
         if self.pred == None:
             self.pred = Vehicle()
         
-        self.pred.append_state(msg)
+        self.pred.x.append(msg.x)
+        self.pred.y.append(msg.y)
+        self.pred.heading.append(msg.psi)
+        self.pred.v.append(msg.v)
 
         self.pred.delta.append(msg.df)
         self.pred.a.append(msg.a)
 
-    def slicing_states(self, start_idx, end_idx):
+    def slicing_data(self, start_time, end_time):
         """
         take a slice of the time and states
         """
-        self.t0 = self.t[start_idx]
-        self.t = [x-self.t0 for x in self.t[start_idx: end_idx]]
+        # For State
+        start_time_diff = np.abs(np.array(self.state_t) - start_time)
+        start_idx = np.argmin(start_time_diff)
+
+        end_time_diff = np.abs(np.array(self.state_t) - end_time)
+        end_idx = np.argmin(end_time_diff)
+
+        self.state_t0 = self.state_t[start_idx]
+        self.state_t = [
+            x-self.state_t0 for x in self.state_t[start_idx: end_idx]]
 
         self.x = self.x[start_idx: end_idx]
         self.y = self.y[start_idx: end_idx]
         self.heading = self.heading[start_idx: end_idx]
         self.v = self.v[start_idx: end_idx]
+
+        # For input
+        start_time_diff = np.abs(np.array(self.input_t) - start_time)
+        start_idx = np.argmin(start_time_diff)
+
+        end_time_diff = np.abs(np.array(self.input_t) - end_time)
+        end_idx = np.argmin(end_time_diff)
+
+        self.input_t0 = self.input_t[start_idx]
+        self.input_t = [
+            x-self.input_t0 for x in self.input_t[start_idx: end_idx]]
+
+        self.delta = self.delta[start_idx: end_idx]
+        self.a = self.a[start_idx: end_idx]
 
 class FiniteStateMachine(object):
     """
@@ -109,20 +137,6 @@ class FiniteStateMachine(object):
         self.states.append(state)
 
         idx = self.state_names.index(state)
-        # if state == "Free-Driving":
-        #     idx = 0
-        # elif state == "Safe-Confidence":
-        #     idx = 1
-        # elif state == "Safe-Yield":
-        #     idx = 2
-        # elif state == "Safe-Infeasible":
-        #     idx = 3
-        # elif state == "HOBCA-Unlocked":
-        #     idx = 4
-        # elif state == "HOBCA-Locked":
-        #     idx = 5
-        # elif state == "Emergency-Break":
-        #     idx = 6
 
         self.state_idxs.append(idx)
 
@@ -153,7 +167,6 @@ class FiniteStateMachine(object):
         self.score_r = self.score_r[start_idx:end_idx]
         self.score_y = self.score_y[start_idx:end_idx]
 
-
 def extract_traj(bag):
     """
     Extract traj and FSM from the bag
@@ -165,8 +178,8 @@ def extract_traj(bag):
     if len(topics) == 0:
         raise ValueError('\tNothing recorded in this bag\n')
     else:
-        # print("Topics contained:", topics)
-        print("Number of topics:", len(topics))
+        print("Topics contained:", topics)
+        # print("Number of topics:", len(topics))
 
     # Initialize Dict for FSM and EV, TV
     FSM = FiniteStateMachine()
@@ -183,20 +196,17 @@ def extract_traj(bag):
 
     # Current States
     for _, msg, t in b.read_messages('/ego_vehicle/est_states'):
-        EV.append_time(t.to_sec())
-        EV.append_state(msg)
-    
+        EV.append_state(msg, t.to_sec())
     
     for _, msg, t in b.read_messages('/target_vehicle/est_states'):
-        TV.append_time(t.to_sec())
-        TV.append_state(msg)
+        TV.append_state(msg, t.to_sec())
 
     # Current Inputs
-    for _, msg, _ in b.read_messages('/ego_vehicle/ecu'):
-        EV.append_input(msg)
+    for _, msg, t in b.read_messages('/ego_vehicle/ecu'):
+        EV.append_input(msg, t.to_sec())
 
-    for _, msg, _ in b.read_messages('/target_vehicle/ecu'):
-        TV.append_input(msg)
+    for _, msg, t in b.read_messages('/target_vehicle/ecu'):
+        TV.append_input(msg, t.to_sec())
 
     # Predicted States
     for _, msg, _ in b.read_messages('/ego_vehicle/pred_states'):
@@ -233,26 +243,30 @@ def cut_static_traj(FSM, EV, TV, threshold=1e-1):
     while EV.v[EV_start_idx] < threshold:
         EV_start_idx += 1
 
-    EV_start_time = EV.t[EV_start_idx]
+    EV_start_time = EV.state_t[EV_start_idx]
     
     EV_end_idx = len(EV.v) - 1
     while EV.v[EV_end_idx] < threshold:
         EV_end_idx -= 1
 
-    EV_end_time = EV.t[EV_end_idx]
+    EV_end_time = EV.state_t[EV_end_idx]
 
-    EV.slicing_states(EV_start_idx, EV_end_idx)
+    EV.slicing_data(EV_start_time, EV_end_time)
     FSM.slicing_data(EV_start_time, EV_end_time)
 
     TV_start_idx = 0
     while TV.v[TV_start_idx] < threshold:
         TV_start_idx += 1
 
+    TV_start_time = TV.state_t[TV_start_idx]
+
     TV_end_idx = len(TV.v) - 1
     while TV.v[TV_end_idx] < threshold:
         TV_end_idx -= 1
 
-    TV.slicing_states(TV_start_idx, TV_end_idx)
+    TV_end_time = TV.state_t[TV_end_idx]
+
+    TV.slicing_data(TV_start_time, TV_end_time)
 
     print("The static data are cut.")
 
